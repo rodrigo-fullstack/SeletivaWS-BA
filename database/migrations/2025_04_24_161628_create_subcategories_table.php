7959cec61af802847c41e06deb64285721eb1402
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-24 15:04:13 -0300

    refactor: Refatorar categorias

--- database/migrations/2025_04_24_161628_create_subcategories_table.php
+++ database/migrations/2025_04_24_161628_create_subcategories_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('subcategory', function (Blueprint $table) {
+        Schema::create('subcategories', function (Blueprint $table) {
             $table->id();
             $table->string('nome_subcategoria', 100);
             $table->timestamps();
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('subcategory');
+        Schema::dropIfExists('subcategories');
     }
 };

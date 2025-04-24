d1ba292e3b52ea08c5d57a4b9af1ed389c7dea24
rodrigo-fullstack <rodrigopereirafaria9@gmail.com>
2025-04-24 15:05:09 -0300

    feat(seed): Semear categorias no bd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Subcategory;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -31,5 +33,34 @@ class DatabaseSeeder extends Seeder
                 'updated_at' => now(),
             ]
         ]);
+
+        Category::insert(
+            [
+                [
+                    'nome_categoria' => 'Teste Categoria 1',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ],
+                [
+                    'nome_categoria' => 'Teste Categoria 2',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]
+            ]
+            );
+        Subcategory::insert(
+            [
+                [
+                    'nome_subcategoria' => 'Teste SubCategoria 1',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ],
+                [
+                    'nome_subcategoria' => 'Teste SubCategoria 2',
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]
+            ]
+            );
     }
 }
